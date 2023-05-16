3a52b73c3c8fc8e4484532682f555131c1673680
EunjinKo <eunjin1@fnfcorp.com>
2023-05-16 20:59:30 +0900

    fst template

--- kokoo/views.py
+++ kokoo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Template, TextField, SelectField, Selection
+from django.utils import timezone
 
 # Create your views here.
+def template_list(request):
+    templates = Template.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'kokoo/template_list.html', {'templates':templates})
